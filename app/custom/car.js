1654ed4195d00bace8f29a8d8e9aae8921f9a807
LeopoldHock <leopoldhock@outlook.com>
2021-10-15 15:52:31 +0200

    added comments

--- app/custom/car.js
+++ app/custom/car.js
@@ -7,17 +7,27 @@ export default class Car {
     @service store;
 
     constructor({ data, context } = {}) {
+        // this will enable service injection
         setOwner(this, getOwner(context));
+        // this will copy all property keys from the original model object
         set(this, "keys", Object.keys(data));
         for (let key of this.keys) {
             set(this, key, data[key]);
         }
     }
 
-    changeColor(colorName) {
+    changeColor(colorName, { store } = {}) {
         console.log("Now attempting to retrieve the object from inside the tracked object...");
-        let color = this.store.peekRecord("color", colorName);
+        let color;
+        if (store) {
+            // When the store is handed over from the calling context, it works
+            color = store.peekRecord("color", colorName);
+        } else {
+            // When the store is accessed through injection, it does not work
+            color = this.store.peekRecord("color", colorName);
+        }
         console.log("Changing color to " + color.name + "...");
         set(this, "color", color.name);
+        console.log("Color successfully changed.");
     }
 }
--- app/services/car-service.js
+++ app/services/car-service.js
@@ -11,18 +11,24 @@ export default class CarService extends Service {
 
     start() {
         console.log("Starting the test...");
+        // 'cars' and 'colors' have been loaded in the route's model hook
         let cars = this.store.peekAll("car");
         let that = this;
-        cars.forEach(element => {
-            let carCopy = element.createInstance();
+        // we will create custom objects of the type 'class' from each 'car' record. The method to do that is defined on the model class
+        cars.forEach(record => {
+            let carCopy = record.createInstance();
             that.data.cars.push(carCopy);
             console.log("Added the " + carCopy.color + " car to the array.");
         });
         console.log("Attempting to paint the first car yellow...");
+        // First test: accessing the 'yellow' color record in this context works
         let color = this.store.peekRecord("color", "yellow");
         if (color) {
             console.log("The color object was successfully retrieved from the store.");
+            // We will call a method on the 'car' object that tries to access the store via injection --> fails
             this.data.cars[0].changeColor("yellow");
+            // Alternatively, we can hand over the store to the 'car' object --> works
+            //this.data.cars[0].changeColor("yellow", { store: this.store });
         }
     }
 }
